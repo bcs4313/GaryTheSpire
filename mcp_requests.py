1433203a1ac1b5d1b9da6541cbeee6f741b4ca98
bcs4313 <bcs4313@g.rit.edu>
2026-06-20 03:23:13 -0400

    Accounting for elites

--- mcp_requests.py
+++ mcp_requests.py
@@ -43,7 +43,7 @@ def play_first_card():
         print("Vakku: failed to find your game state. Why do you disappoint him?")
     else:
         # base case 2: not fighting
-        if game_state["state_type"] != "monster":
+        if game_state["state_type"] != "monster" and game_state["state_type"] != "elite":
             print("Vakku: Your are not fighting a monster right now. You test my patience mortal.")
 
         # get hand
@@ -87,7 +87,7 @@ def can_play_first_card():
         return False
     else:
         # base case 2: not fighting
-        if game_state["state_type"] != "monster":
+        if game_state["state_type"] != "monster" and game_state["state_type"] != "elite":
             return False
 
         # base case 3: no hand or player
@@ -128,4 +128,5 @@ def vakku_simulator():
             perform_action("end_turn", {})
 
 if __name__ == "__main__":
+    get_full_game_state()
     vakku_simulator()
